f39d686ca7fcb39e592fb50e89fe4a752544492f
unknown <pedro.braga@ccc.ufcg.edu.br>
2018-05-29 20:53:39 -0300

    Testes shortestPath e connected*

--- srcTest/graphtest/SCCTest.java
+++ srcTest/graphtest/SCCTest.java
@@ -1,14 +1,33 @@
 package graphtest;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-public class SCCTest {
+import graph.p1.Graph;
+import graph.p1.GraphImpl;
 
+public class SCCTest {
+	
+	/*
+	 * OBSERVACAO: O método "Connected" nao foi implementado pelo grupo.
+	 * Os requisitos foram alterados e com isso o método "SCC" passou a
+	 * não fazer mais parte dos requisitos, entrando o "connected" em seu lugar.
+	 */
+	
+	
+	
+	@Before
+	public void setUp() {
+	
+	}
+	
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
 	}
 
 }
--- srcTest/graphtest/ShortestPathTest.java
+++ srcTest/graphtest/ShortestPathTest.java
@@ -1,14 +1,86 @@
 package graphtest;
 
+
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import graph.p1.Graph;
+import graph.p1.GraphImpl;
+import graph.p1.Vertex;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
 
 public class ShortestPathTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	Graph<Integer> grafoComPeso = new GraphImpl<>();
+	Graph<Integer> grafoSemPeso = new GraphImpl<>();
+	Graph<Integer> grafoSemPeso2 = new GraphImpl<>();
+	Graph<Integer> grafoComArestaNegativa = new GraphImpl<>();
+	
+	List<Vertex<Integer>> verticesGrafoSemPeso;
+	List<Vertex<Integer>> verticesGrafoComPeso;
+	List<Vertex<Integer>> verticesGrafoSemPeso2;
+	List<Vertex<Integer>> verticesGrafoComArestaNegativa;
+	
+	final String path = "C:\\Users\\Pedro\\Desktop\\pratica2\\graph_txt\\";
+	//(properties -> location)
+	
+	
+	
+	@Before
+	public void setUp() {
+		try {
+			grafoComPeso.readGraph(path + "grafoComPeso.txt"); 
+			verticesGrafoComPeso = grafoComPeso.getVertices();
+			
+			grafoSemPeso.readGraph(path + "grafoSemPeso.txt");
+			verticesGrafoSemPeso = grafoSemPeso.getVertices();
+			
+			grafoSemPeso2.readGraph(path + "grafoSemPeso2.txt");
+			verticesGrafoSemPeso2 = grafoSemPeso2.getVertices();
+			
+			grafoComArestaNegativa.readGraph(path + "grafoComArestasNegativas.txt");
+			verticesGrafoComArestaNegativa = grafoComArestaNegativa.getVertices();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
+	
+	@Test
+	public void testGrafoArestasNegativas() {
+		Assert.assertEquals("4 3 2 1 ", grafoComArestaNegativa.shortestPath(verticesGrafoComArestaNegativa.get(3),
+				verticesGrafoComArestaNegativa.get(0)));
+	}
+	
+	@Test
+	public void testGrafoSemPeso() {
+		Assert.assertEquals("1 5 4", grafoSemPeso.shortestPath(verticesGrafoSemPeso.get(0), verticesGrafoSemPeso.get(4))); 
+		
+	}
+	
+	@Test
+	public void testGrafoComPeso() {
+		Assert.assertEquals("1 2 5 4 3", grafoComPeso.shortestPath(verticesGrafoComPeso.get(0), 
+				verticesGrafoComPeso.get(3))); 
+	}
+	
+	@Test
+	public void secondTestGrafoComPeso() {
+		Assert.assertEquals("1 2 5 ", grafoComPeso.shortestPath(verticesGrafoComPeso.get(0), 
+				verticesGrafoComPeso.get(2))); 
+	}
+	
+	@Test
+	public void testGrafoSemPeso2() {
+		Assert.assertEquals("3 2 4", grafoSemPeso2.shortestPath(verticesGrafoSemPeso2.get(2), verticesGrafoSemPeso2.get(3)));
+	}
+	
+	
 }
