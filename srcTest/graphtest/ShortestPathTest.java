c03e5da35c1fb9a87909400f94000df654783fba
unknown <pedro.braga@ccc.ufcg.edu.br>
2018-05-29 21:12:02 -0300

    mais um teste

--- srcTest/graphtest/ShortestPathTest.java
+++ srcTest/graphtest/ShortestPathTest.java
@@ -22,11 +22,15 @@ public class ShortestPathTest {
 	Graph<Integer> grafoSemPeso = new GraphImpl<>();
 	Graph<Integer> grafoSemPeso2 = new GraphImpl<>();
 	Graph<Integer> grafoComArestaNegativa = new GraphImpl<>();
+	Graph<Integer> grafoComArestaNegativa2 = new GraphImpl<>();
+
 	
 	List<Vertex<Integer>> verticesGrafoSemPeso;
 	List<Vertex<Integer>> verticesGrafoComPeso;
 	List<Vertex<Integer>> verticesGrafoSemPeso2;
 	List<Vertex<Integer>> verticesGrafoComArestaNegativa;
+	List<Vertex<Integer>> verticesGrafoComArestaNegativa2;
+
 	
 	final String path = "C:\\Users\\Pedro\\Desktop\\pratica2\\graph_txt\\";
 	//(properties -> location)
@@ -47,6 +51,9 @@ public class ShortestPathTest {
 			
 			grafoComArestaNegativa.readGraph(path + "grafoComArestasNegativas.txt");
 			verticesGrafoComArestaNegativa = grafoComArestaNegativa.getVertices();
+			
+			grafoComArestaNegativa2.readGraph(path + "grafoComArestasNegativas2.txt");
+			verticesGrafoComArestaNegativa2 = grafoComArestaNegativa2.getVertices();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -58,6 +65,8 @@ public class ShortestPathTest {
 		Assert.assertEquals("4 3 2 1 ", grafoComArestaNegativa.shortestPath(verticesGrafoComArestaNegativa.get(3),
 				verticesGrafoComArestaNegativa.get(0)));
 	}
+	
+	
 	
 	@Test
 	public void testGrafoSemPeso() {
@@ -80,6 +89,15 @@ public class ShortestPathTest {
 	@Test
 	public void testGrafoSemPeso2() {
 		Assert.assertEquals("3 2 4", grafoSemPeso2.shortestPath(verticesGrafoSemPeso2.get(2), verticesGrafoSemPeso2.get(3)));
+	}
+	
+	@Test
+	public void testGrafoComArestaNegativa2() {
+		for (Vertex v : this.verticesGrafoComArestaNegativa2) {
+			System.out.println(v.getData());
+		}
+		Assert.assertEquals("1 2 3 5", grafoComArestaNegativa2.shortestPath(verticesGrafoComArestaNegativa2.get(0), 
+				verticesGrafoComArestaNegativa2.get(4)));
 	}
 	
 	
