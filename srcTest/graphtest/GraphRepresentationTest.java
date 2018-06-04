f9a7e4b2fd4b77bc9ce4bc430fbf790b1986555e
DamiaoRobson <damiao.domiciano@ccc.ufcg.edu.br>
2018-06-04 01:44:55 -0300

    Implementação de testes ReadGraph

--- srcTest/graphtest/GraphRepresentationTest.java
+++ srcTest/graphtest/GraphRepresentationTest.java
@@ -5,16 +5,46 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import graph.p1.Graph;
+import graph.p1.GraphImpl;
+
 
 public class GraphRepresentationTest {
 
-	@Before
-	public void setUp(){
-	}
+	private Graph<Integer> graph1;
+	private Graph<Integer> graph2;
+	private Graph<Integer> graph3;
+	private Graph<Integer> graph4;
+	private Graph<Integer> graph5;
 	
+	
+
+	private final String PATH = "graph_txt/";
+	private final String NAO_EXECUTAR = "NAO EXECUTAR ESSA LINHA";
+
+	@Before
+	public void setUp() {
+		try {
+			graph1 = new GraphImpl<>();
+			graph2 = new GraphImpl<>();
+			graph3 = new GraphImpl<>();
+			graph4 = new GraphImpl<>();
+			graph5 = new GraphImpl<>();
+			
+			//graph1.readGraph(PATH + "grafoSemPeso.txt");
+			graph2.readWeightedGraph(PATH + "grafoComPeso.txt");
+			graph3.readGraph(PATH + "grafoComArestasNegativas.txt");
+			graph4.readWeightedGraph(PATH + "grafoComPesoNegativo.txt");
+			graph5.readGraph(PATH + "grafoExtenco.txt");
+			
+		} catch (Exception e) {
+			fail(NAO_EXECUTAR);
+		}
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void grafoSemPesoMATest() {
+		System.out.println(graph1.graphRepresentation(PATH + "grafoSemPeso.txt"));
 	}
 
 }
--- srcTest/graphtest/ReadGraphTest.java
+++ srcTest/graphtest/ReadGraphTest.java
@@ -2,18 +2,104 @@ package graphtest;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import graph.p1.Graph;
 import graph.p1.GraphImpl;
 
-
-
 public class ReadGraphTest {
 
-	@Test
-	public void test() {
-		
+	private Graph<Integer> graph;
+	private Graph<String> graph2;
+	private Graph<Double> graph3;
+	
+
+	private final String PATH = "graph_txt/";
+	private final String NAO_EXECUTAR = "NAO EXECUTAR ESSA LINHA";
+
+	@Before
+	public void setUp() {
+		try {
+			graph = new GraphImpl<>();
+			graph2 = new GraphImpl<>();
+			graph3 = new GraphImpl<>();
+		} catch (Exception e) {
+			fail(NAO_EXECUTAR);
+		}
 	}
 
+	@Test
+	public void readGraphSemPesoTest() {
+		try {
+			graph.readGraph(PATH + "grafoSemPeso.txt");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			fail(NAO_EXECUTAR);
+		}
+	}
+
+	@Test
+	public void readGraphComPesoTest() {
+		try {
+			graph.readWeightedGraph(PATH + "grafoComPeso.txt");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			fail(NAO_EXECUTAR);
+		}
+	}
+	
+	@Test
+	public void readGraphArestaNegativaTest() {
+		try {
+			graph.readGraph(PATH + "grafoComArestasNegativas.txt");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			fail(NAO_EXECUTAR);
+		}
+	}
+	
+	@Test
+	public void grafoComPesoNegativoTest() {
+		try {
+			graph.readWeightedGraph(PATH + "grafoComPesoNegativo.txt");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			fail(NAO_EXECUTAR);
+		}
+	}
+	
+	@Test
+	public void grafoArestaLetraTest() {
+		try {
+			graph2.readGraph(PATH + "grafoArestaLetra.txt");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			fail(NAO_EXECUTAR);
+		}
+	}
+	
+	@Test
+	public void grafoArestaDoubleTest() {
+		try {
+			graph3.readGraph(PATH + "grafoArestaDouble.txt");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			fail(NAO_EXECUTAR);
+		}
+	}
+	
+	@Test
+	public void grafoExtencoTest() {
+		try {
+			graph.readGraph(PATH + "grafoExtenco.txt");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			fail(NAO_EXECUTAR);
+		}
+	}
 }
